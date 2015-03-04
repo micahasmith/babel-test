ebe1583ee11d02cc9811a174c54f98579ab99e0f
Micah Smith <micah.a.smith@gmail.com>
2015-03-04 08:47:08 -0500

    new tests

--- test/app.test.js
+++ test/app.test.js
@@ -45,3 +45,52 @@ describe('swapping out deps v2',function(){
 		expect(index(2,3)).toEqual( 2+3 );
 	});
 });
+
+
+describe('swapping out deps v3',function(){
+	var index;
+	beforeEach(function(done) {
+		//delete old ones first
+		System.delete("src/triple");
+		System.delete("src/double");
+		System.delete("src/index");
+
+		// import replacements
+		System.set("src/double",System.get("test/fakes/identity"));
+		System.set("src/triple",System.get("test/fakes/identity"));
+
+		// import src/index, hopefully with new funcs
+		System.import('src/index').then(function(ind) {
+			index = ind.default;
+			done();
+		});
+	});
+
+	it('works',function() {
+		expect(index(2,3)).toEqual( 2+3 );
+	});
+});
+
+describe('swapping out deps v4',function(){
+	var index;
+	beforeEach(function(done) {
+		//delete old ones first
+		System.delete("src/triple");
+		System.delete("src/double");
+		System.delete("src/index");
+
+		// import replacements
+		System.map["src/triple"] = "test/fakes/identity";
+		System.map["src/double"] = "test/fakes/identity";
+
+		// import src/index, hopefully with new funcs
+		System.import('src/index').then(function(ind) {
+			index = ind.default;
+			done();
+		});
+	});
+
+	it('works',function() {
+		expect(index(2,3)).toEqual( 2+3 );
+	});
+});
