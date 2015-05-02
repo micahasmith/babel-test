f42f28e4b7121677460dfce526f11c688daa142c
Eric Drechsel <eric@pdxhub.org>
2015-05-02 12:10:29 -0700

    Update tooling, prune unused deps, and add babel via jspm

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
 System.config({
   "baseURL": "/",
   "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
   "paths": {
-    "*": "*.js"
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
   },
   "bundles": {
     "build": [
@@ -10,6 +17,23 @@ System.config({
       "src/triple",
       "src/index"
     ]
+  }
+});
+
+System.config({
+  "map": {
+    "babel": "npm:babel-core@5.2.6",
+    "babel-runtime": "npm:babel-runtime@5.2.6",
+    "core-js": "npm:core-js@0.9.6",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:core-js@0.8.4": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@0.9.6": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
   }
 });
 
